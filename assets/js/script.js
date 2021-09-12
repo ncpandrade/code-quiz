819aec37de2efab61ff3f5c62ee101373aed8a7f
Nadine <nadine_paez@hotmail.com>
2021-09-12 10:07:20 -0400

    added timer, started function start quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 var questions = [
     { 
         question: "Who invented JavaScript?",
-        choice: ['Elon Musk', 'Steve Jobs','Bill gates','Brendan Eich'],
+        choice: ['Elon Musk', 'Steve Jobs','Bill Gates','Brendan Eich'],
         answer: 'Brendan Eich'
     }
     //add more questions here; use same format or use true or false
@@ -42,9 +42,34 @@ var questions = [
 
     //define elements - when button is clicked the quiz function starts
     startEl.addEventListener('click', startQuiz);
+    //define timer start
+    timerEl.addEventListener('click', countdown)
 
 //timer will begin
+// Timer that counts down from 60
+function countdown() {
+  var timeLeft = 60;
 
+  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var timeInterval = setInterval(function() {
+    // As long as the `timeLeft` is greater than 1
+    if (timeLeft > 1) {
+      // Set the `textContent` of `timerEl` to show the remaining seconds
+      timerEl.textContent = timeLeft;
+      // Decrement `timeLeft` by 1
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+      timerEl.textContent = timeLeft;
+      timeLeft--;
+    } else {
+      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+      timerEl.textContent = '';
+      // Use `clearInterval()` to stop the timer
+      clearInterval(timeInterval);
+    }
+  }, 1000);
+}
 //questions array
 
 //global selectors to reference your DOM elements
@@ -57,7 +82,5 @@ var questions = [
 function startQuiz(){
   var startScreen = document.getElementById('start-screen-id');
   startScreen.setAttribute('style', 'display:none');
-  console.log('HELLO');
+  questionsEl.textContent = questions[0];
 }
-
-// set attribute - uses 2 parameters
